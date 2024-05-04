1960313585a61d25a5cb49cc042c19e2d04e2c2d
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2024-05-04 18:17:22 -0400

    added goose

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,17 +1,22 @@
 import "./Landing.scss";
+// company logos
 import Google from "../assets/logos/google.png";
 import GeeksforGeeks from "../assets/logos/GeeksForGeeks.png";
 import echo3D from "../assets/logos/echo3D.png";
-
 import KPMG from "../assets/logos/kpmg.png";
 import EY from "../assets/logos/ey.png";
 import Deloitte from "../assets/logos/deloitte.png";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
+import CIBC from "../assets/logos/CIBC.png";
+import TD from "../assets/logos/TD.png";
+import Scotiabank from "../assets/logos/scotiabank.png";
+
+import Goose from "../assets/goose.png";
 
 export default function Landing() {
   return (
-    <div>
+    <div className="landing_container">
       <Navbar />
       <div className="p-4">
         <Header
@@ -19,6 +24,7 @@ export default function Landing() {
           subtitle="Prep for your dream company"
         />
       </div>
+
       <div className="landing">
         <div className="box_container">
           <div className="box">
@@ -65,11 +71,47 @@ export default function Landing() {
           </div>
         </div>
         <div className="box_container">
-          <div className="box">
-            <p>Technology:</p>
+          <div className="box_2">
+            <a href="/customquestions">
+              <div className="company">
+                {/* fix button */}
+                <div className="text_container">
+                  <p>Make your own questions</p>
+                </div>
+              </div>
+            </a>
+            <div className="goose_container">
+              <div className="dialogue-desktop">
+                <img className="goose" src={Goose} alt="Goose" />
+                <div className="dialogue-bubble">
+                  <p>
+                    Welcome to QuackPrep!
+                    <br /> Choose a company to get started!
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <p>Technology:</p>
+          <div className="box">
+            <p>Bank:</p>
+            <div className="company">
+              <img src={CIBC} alt="CIBC" />
+              <div className="text_container">
+                <p>CIBC</p>
+              </div>
+            </div>
+            <div className="company">
+              <img src={TD} alt="TD" />
+              <div className="text_container">
+                <p>TD</p>
+              </div>
+            </div>
+            <div className="company">
+              <img src={Scotiabank} alt="Scotiabank" />
+              <div className="text_container">
+                <p>Scotiabank</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
