7e2e9f9e0dd45957e60ead6aadec70d5a19d5a1d
Inqiyad <inqiyad18@gmail.com>
2024-05-04 15:59:33 -0400

    Navbar

--- src/pages/CustomQuestions.js
+++ src/pages/CustomQuestions.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Navbar from "../components/Navbar";
 
 export default function CustomQuestions() {
   const [inputFields, setInputFields] = useState([{ value: "" }]);
@@ -13,28 +14,36 @@ export default function CustomQuestions() {
     setInputFields(values);
   };
 
+  const handleSend = () => {
+    
+  }
+
   return (
-    <div className="p-4 mt-12">
-      {inputFields.map((inputField, index) => (
-        <div key={index} className="input-group mb-4">
-          <input
-            type="text"
-            className="border rounded-lg px-4 py-2 w-full h-16"
-            value={inputField.value}
-            onChange={(event) => handleInputChange(index, event)}
-          />
+    <div>
+      <Navbar />
+      <div className="p-4 mt-12">
+        {inputFields.map((inputField, index) => (
+          <div key={index} className="input-group mb-4">
+            <input
+              type="text"
+              className="placeholder:italic placeholder:text-slate-400 border rounded-lg px-4 py-2 w-full h-16"
+              placeholder="Type your question..."
+              value={inputField.value}
+              onChange={(event) => handleInputChange(index, event)}
+            />
+          </div>
+        ))}
+        <div className="flex justify-end">
+          <button
+            className="bg-customblue hover:bg-blue-300 text-black py-2 px-4 rounded mx-2"
+            onClick={handleAddInput}
+          >
+            Add Another Question
+          </button>
+          <button className="bg-customblue hover:bg-blue-300 text-black py-2 px-4 rounded mx-2">
+            Send to Mr Goose
+          </button>
         </div>
-      ))}
-      <div className="flex justify-end">
-        <button
-          className="bg-customblue hover:bg-blue-300 text-black font-bold py-2 px-4 rounded mx-2"
-          onClick={handleAddInput}
-        >
-          Add Another Question
-        </button>
-        <button className="bg-customblue hover:bg-blue-300 text-black font-bold py-2 px-4 rounded mx-2">
-          Send to Mr Goose
-        </button>
       </div>
     </div>
   );
